107e078fee9f07d8de4e956aa18a33811a9a9070
curio._.siti <70625251+Monishkumarvr@users.noreply.github.com>
2024-09-04 21:26:40 +0530

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('aiForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const email = this.querySelector('input[type="email"]').value;
-    const industry = this.querySelector('select').value;
-    console.log('Submitted:', { email, industry });
-    this.reset(); // Reset form after submission
-});
